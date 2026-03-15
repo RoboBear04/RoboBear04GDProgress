db70e134bed294cc6585ed27e4bbd317d3efc239
Rob Ferguson <116104045+RoboBear04@users.noreply.github.com>
2026-03-15 17:22:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function openTab(tabName) {
   let tabs = document.getElementsByClassName("tab-content");
 
-for (let i = 0; i < tabs.length; i++) {
-  tabs[i].style.display = "none";
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
   }
 
-  document.getElementByID(tabName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
 }
 
 window.onload = function() {
